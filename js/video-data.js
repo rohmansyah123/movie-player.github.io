7d5043e105daa0c77b4e8ce0bf12ee1c3e28a175
ROHMANSYAH <40875901+rohmansyah123@users.noreply.github.com>
2025-06-11 02:31:44 +0700

    Update and rename video-loader.js to video-data.js

---
+++ js/video-data.js
@@ -0,0 +1,25 @@
+// js/video-data.js
+
+const videoPlaylist = [
+    {
+        id: 'video1',
+        title: 'Pengantar Teknologi Web',
+        url: 'https://short.icu/v6oUMlhx2' // Ganti dengan link video Anda
+    },
+    {
+        id: 'video2',
+        title: 'Tutorial Dasar HTML & CSS',
+        url: 'https://www.youtube.com/embed/dQw4w9WgXcQ' // Contoh link video YouTube
+    },
+    {
+        id: 'video3',
+        title: 'Pengenalan JavaScript',
+        url: 'https://player.vimeo.com/video/84107198' // Contoh link video Vimeo
+    },
+    {
+        id: 'video4',
+        title: 'Tips & Trik Efisiensi Coding',
+        url: 'https://short.icu/v6oUMlhx2' // Contoh link video lainnya
+    }
+    // Anda bisa menambahkan lebih banyak objek video di sini
+];
